19e4869e534ae7a15e881ca97adf2458593d4268
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-03-28 16:29:46 -0700

    wip: styled 3 different ways

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 
 import './App.css';
 import Button1 from './components/example-1-normal-css/Button';
+import Button2 from './components/example-2-inline-css/Button';
+import Button3 from './components/example-3-styled-components/Button';
+
 // Later on, we'll import button components here
 
 const App = () => {
@@ -11,7 +14,10 @@ const App = () => {
         // Add all example components:
         <div className="App">
         <Button1 buttonText="Example 1" />
-        //...
+        <Button2 buttonText="Example 2" />
+        <Button3 buttonText="Example 3" />
+
+
         </div>
       }
     </div>
